013b8ea9439a119d7000784edeb356111ce72982
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2022-09-23 00:20:29 +0200

    Improved readability

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,41 @@ use lib::{client, server};
 
 #[derive(Parser)]
 struct Cli {
-    #[clap(short = 's', long = "server")]
+    #[clap(long = "server")]
     server: Option<bool>,
 
     #[clap(short = 'p', long = "port")]
     port: Option<String>,
+
+    #[clap(short = 's', long = "size")]
+    board_size: Option<usize>,
 }
 
 fn main() {
     let args = Cli::parse();
+    let board_size = match args.board_size {
+        Some(s) => s,
+        None => 3,
+    };
+    let port = match args.port {
+        Some(p) => p,
+        None => String::from("3333"),
+    };
 
-    match args.server {
-        Some(b) => {
-            if b {
-                server::run(3);
-            } else {
-                let port = match args.port {
-                    Some(p) => p,
-                    None => String::from("3333"),
-                };
-                client::run(port);
-            }
-        }
+    let run_server = match args.server {
+        Some(b) => b,
         None => {
             let opt = Select::new("Select one:", vec!["Create a game", "Join a game"])
                 .prompt()
                 .unwrap_or_default();
 
-            if opt.eq("Create a game") {
-                server::run(3);
-            } else {
-                let port = match args.port {
-                    Some(p) => p,
-                    None => String::from("3333"),
-                };
-
-                client::run(port);
-            }
+            opt.eq("Create a game")
         }
+    };
+
+    if run_server {
+        server::run(board_size);
+    } else {
+        client::run(port);
     }
 }
