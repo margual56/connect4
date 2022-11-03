b795fbbc004f7348416e47062cd783400afae922
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2022-11-03 23:05:39 +0100

    Made the change to use lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod board;
+mod chip;
+pub mod client;
+mod errors;
+pub mod server;
+
+// pub use board::{Board, CHIPS_IN_A_ROW};
+// pub use crate::chip::Chip;
+// pub use crate::errors::BoardError;
--- src/lib/mod.rs
+++
@@ -1,9 +0,0 @@
-mod board;
-mod chip;
-pub mod client;
-mod errors;
-pub mod server;
-
-pub use board::{Board, CHIPS_IN_A_ROW};
-pub use chip::Chip;
-pub use errors::BoardError;
